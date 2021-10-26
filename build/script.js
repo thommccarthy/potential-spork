19e33181d731a4412cba9bea3376e92922b7983d
thommccarthy <mccarttj@gmail.com>
2021-10-26 16:39:25 -0400

    smoothscroll script

--- build/script.js
+++ build/script.js
@@ -218,3 +218,21 @@ slides.forEach((slide) => {
     }
   }
 });
+
+// smoothscroll
+const links = document.querySelectorAll('.scroll');
+
+for (const link of links) {
+  link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: 'smooth',
+  });
+}
